20bd20738cca522ccbba28b7013bfc20df8e00de
AIY7788 <aiykhounnakhon@gmail.com>
2025-12-29 18:06:08 +0700

    Fix format

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,34 @@ function App() {
     <>
       <div className="hero-section">
         <h1>Simple, traffic-based pricing</h1>
-        <p>
-          Sing-up for our 30-day trial.
-          No credit card required
-        </p>
+        <p>Sing-up for our 30-day trial. No credit card required</p>
       </div>
 
       <div className="page-views">
-        <h2>100k PAGVIEWS</h2>
+        <h2>100k pageviews</h2>
         <div className="range-section">
           <input
             type="range"
-            className='slice-range'
+            className="slice-range"
             min={0}
             max={100}
             // value={20}
           />
         </div>
 
-        <p className='price'>
-          <span>$16.00</span> / month
+        <p>
+          <span className="price">$16.00</span> / month
         </p>
 
         <div className="billing">
           <p>Monthly Billing</p>
           <label className="toggle-section">
             <input type="checkbox" />
-            <span className='slice-toggle'></span>
+            <span className="slice-toggle"></span>
           </label>
-          <p>Yearly Billing <span>-25%</span></p>
+          <p>
+            Yearly Billing <span>-25%</span>
+          </p>
         </div>
 
         <hr />
